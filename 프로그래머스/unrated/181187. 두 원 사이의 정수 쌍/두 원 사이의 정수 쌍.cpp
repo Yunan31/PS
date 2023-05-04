f7a6867290009862eb792d7e86684d2c2e95279c
유난 <93152240+Yunan31@users.noreply.github.com>
2023-05-04 18:08:03 +0900

    [unrated] Title: 두 원 사이의 정수 쌍, Time: 4.82 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+long long solution(int r1, int r2) {
+    long long answer = 0;
+    long long x4 = 0;
+    
+    for(long long i=1;i<r2;i++){
+        //최대 y 개수에 따라 계산
+        x4 += floor(sqrt(pow(r2, 2) - pow(i, 2)));
+        //안쪽 원에 들어가는 범위를 제거
+        if(i<r1){
+            double inner = sqrt(pow(r1, 2) - pow(i, 2));
+            if(inner/1.00 == (long long)inner) x4++;
+            x4 -= floor(inner);
+        }
+    }
+    answer += x4 * 4;
+    
+    //x, y축 위에 존재하는 개수 더해줌
+    answer += (r2-r1+1) * 4;
+    
+    return answer;
+}
