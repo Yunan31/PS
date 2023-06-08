e8bd462eaf02a53dc6eb9be926c30d3ad4c5c053
유난 <93152240+Yunan31@users.noreply.github.com>
2023-06-09 06:24:13 +0900

    [unrated] Title: 롤케이크 자르기, Time: 1256.74 ms, Memory: 44.3 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.cpp
@@ -1,23 +1,34 @@
 #include <string>
 #include <vector>
-#include <map>
+#include <algorithm>
 
 using namespace std;
 
 int solution(vector<int> topping) {
+    int left[1000005], right[1000005];
     int answer = 0;
-    map<int, int> m1;
-    map<int, int> m2;
-    for (int& i : topping) {
-        m2[i]++;
-    }
-    for (int i = 0; i < topping.size() - 1; i++) {
-        m1[topping[i]]++;
-        m2[topping[i]]--;
-        if (m2[topping[i]] == 0) {
-            m2.erase(topping[i]);
+    vector<int> kind_left, kind_right;
+
+    //왼쪽 순서대로 종류 배열에 담기
+    for(int i=0;i<topping.size();i++){
+        if(find(kind_left.begin(), kind_left.end(), topping[i]) == kind_left.end()){
+            kind_left.push_back(topping[i]);
         }
-        if (m1.size() == m2.size()) answer++;
+        left[i] = kind_left.size();
     }
+
+    //오른쪽 순서대로 종류 배열에 담기
+    for(int i=topping.size()-1;i>=0;i--){
+        if(find(kind_right.begin(), kind_right.end(), topping[i]) == kind_right.end()){
+            kind_right.push_back(topping[i]);
+        }
+        right[i] = kind_right.size();
+    }
+
+    //left와 right 확인해가며 answer++
+    for(int i=0;i<topping.size()-1;i++){
+        if(left[i] == right[i+1]) answer++;
+    }
+
     return answer;
 }
