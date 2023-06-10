aa473778fe07358abb3254a0456c04f7c07b9f8d
유난 <93152240+Yunan31@users.noreply.github.com>
2023-06-10 15:39:32 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  int tile[1005];
+
+  cin >> n;
+
+  tile[1] = 1;
+  tile[2] = 3;
+
+  for(int i=3;i<=n;i++){
+    tile[i] = (tile[i-1] + tile[i-2]*2)%10007;
+  }
+
+  cout << tile[n];
+
+  return 0;
+}
