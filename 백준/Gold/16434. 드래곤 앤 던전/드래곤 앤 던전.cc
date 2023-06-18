e3fc465bf358d33ecda8da1c757c4c4b711d7b5b
유난 <93152240+Yunan31@users.noreply.github.com>
2023-06-18 21:24:36 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 148 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  long long N, atk;
+  long long max_HP, minus_HP = 0;
+  long long room_type, mon_atk, mon_HP;
+  long long portion, increase;
+  long long death_HP = 0;
+  
+  cin >> N >> atk;
+
+  for(int i=0;i<N;i++){
+    cin >> room_type;
+    if(room_type == 1){
+      cin >> mon_atk >> mon_HP;
+      minus_HP += mon_atk*(mon_HP/atk);
+      if(mon_HP%atk == 0) minus_HP -= mon_atk;
+    }
+    else{
+      death_HP = max(death_HP, minus_HP);
+      cin >> increase >> portion;
+      atk += increase;
+      minus_HP -= portion;
+      if(minus_HP < 0) minus_HP = 0;
+    }
+  }
+
+  cout << max(death_HP, minus_HP) + 1;
+
+  return 0;
+}
