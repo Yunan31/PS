fdce3c1dd0bb5e0bffa5f1d2d7c52820e5977662
유난 <93152240+Yunan31@users.noreply.github.com>
2023-06-28 16:59:59 +0900

    [Gold IV] Title: 루머, Time: 1052 ms, Memory: 25160 KB -BaekjoonHub

--- 백준/Gold/19538. 루머/루머.cc
+++ 백준/Gold/19538. 루머/루머.cc
@@ -7,9 +7,10 @@ using namespace std;
 int main() {
   vector<int> around[200005];
   int rumor[200005];
+  int visit[200005] = {0};
+  double condition[200005];
   int N, M;
-  int min_count = 0, minute = 0, next_min = 0;
-  queue<int> Q;
+  queue<pair<int, int>> Q;
   
   fill_n(rumor, 200005, -1);
   cin >> N;
@@ -21,48 +22,30 @@ int main() {
       around[i].push_back(person);
       cin >> person;
     }
+    condition[i] = (double)around[i].size()/2;
   }
 
   cin >> M;
   for(int i=1;i<=M;i++){
     cin >> person;
-    Q.push(person);
+    Q.push({person, 1});
     rumor[person] = 0;
-    min_count++;
+    visit[person] = 1;
   }
 
   while(!Q.empty()){
-    int now = Q.front();
+    int now = Q.front().first;
+    int minute = Q.front().second;
     Q.pop();
-    min_count--;
 
-    //자기 주변인 루머믿는사람 수 계산
-    if(rumor[now] == -1){
-      int count = 0;
-      for(int i=0;i<around[now].size();i++){
-        if(rumor[around[now][i]] != -1 && rumor[around[now][i]] != minute) count++;
+    for(int i=0;i<around[now].size();i++){
+      condition[around[now][i]]--;
+      //cout << around[now][i] << ": " << condition[around[now][i]] << " min: " << minute << endl;
+      if(condition[around[now][i]] <= 0 && !visit[around[now][i]]){
+        Q.push({around[now][i], minute+1 });
+        rumor[around[now][i]] = minute;
+        visit[around[now][i]] = 1;
       }
-      if((double)count/around[now].size() >= 0.5) rumor[now] = minute;
-    }
-
-    if(rumor[now] != -1){
-      for(int i=0;i<around[now].size();i++){
-        if(rumor[around[now][i]] == -1){
-          Q.push(around[now][i]);
-          next_min++;
-        }
-      }
-    }
-
-    if(min_count == 0){
-      min_count = next_min;
-      next_min = 0;
-      minute++;
-
-      // for(int i=1;i<=N;i++){
-      //   cout << rumor[i] << " ";
-      // }
-      // cout << endl;
     }
   }
 
