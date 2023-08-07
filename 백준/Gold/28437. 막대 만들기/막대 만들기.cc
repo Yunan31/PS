01cc18775153fc3fe40bcd883202054874efd4a8
윤환 <93152240+Yunan31@users.noreply.github.com>
2023-08-07 21:39:03 +0900

    [Gold III] Title: 막대 만들기, Time: 728 ms, Memory: 3068 KB -BaekjoonHub

---
+++ 백준/Gold/28437. 막대 만들기/막대 만들기.cc
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[100005] = {0};
+int target[100005];
+int high = 0;
+
+int solve(int n){
+  if(n > high) return 0;
+
+  dp[n]++;
+
+  int tmp = n*2;
+  while(tmp <= high){
+    solve(tmp);
+    tmp+=n;
+  }
+
+  return 0;
+}
+
+int main() {
+  int N, Q;
+  int A[100005];
+
+  cin >> N;
+  for(int i=0;i<N;i++){
+    cin >> A[i];
+  }
+  sort(A, A+N);
+
+  cin >> Q;
+  for(int i=0;i<Q;i++){
+    cin >> target[i];
+    high = max(high, target[i]);
+  }
+
+  for(int i=0;i<N;i++){
+    solve(A[i]);
+  }
+
+  for(int i=0;i<Q;i++){
+    cout << dp[target[i]] << " ";
+  }
+}
