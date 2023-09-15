269cb2991f994de7b5f4037ca1b56b56c261dfa3
윤환 <93152240+Yunan31@users.noreply.github.com>
2023-09-15 14:39:28 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 312 ms, Memory: 4796 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main() {
+  int N, M;
+  vector<int> E[1005];
+  int in_degree[1005] = {0};
+  int visit[1005] = {0};
+  int season = 1;
+  int answer[1005] = {0};
+
+  cin >> N >> M;
+
+  for(int i=0;i<M;i++){
+    int A, B;
+    cin >> A >> B;
+    in_degree[B]++;
+    E[A].push_back(B);
+  }
+
+  queue<int> Q;
+  int count = 0;
+
+  for(int i=1;i<=N;i++){
+    if(in_degree[i] == 0){
+      Q.push(i);
+      answer[i] = season;
+      visit[i]++;
+      count++;
+    }
+  }
+
+  season++;
+
+  int temp = 0;
+  while(!Q.empty()){
+    int from = Q.front();
+    Q.pop();
+
+    for(int i=0;i<E[from].size();i++){
+      int to = E[from][i];
+      if(visit[to] == 0 && --in_degree[to] == 0){
+        visit[to]++;
+        Q.push(to);
+        temp++;
+        answer[to] = season;
+      }
+    }
+
+    count--;
+    if(count == 0){
+      season++;
+      count = temp;
+      temp = 0;
+    }
+  }
+
+  for(int i=1;i<=N;i++){
+    cout << answer[i] << " ";
+  }
+}
