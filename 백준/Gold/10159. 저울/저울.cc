9326a94d50dd9089d72f959eab654764b12aba04
윤환 <93152240+Yunan31@users.noreply.github.com>
2023-09-16 20:03:15 +0900

    [Gold IV] Title: 저울, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int comp[105][105] = {0};
+int N, M;
+
+int main() {
+  cin >> N >> M;
+  for(int i=0;i<M;i++){
+    int w1, w2;
+    cin >> w1 >> w2;
+    comp[w1][w2]++;
+  }
+
+  for(int i=1;i<=N;i++){
+    comp[i][i]++;
+  }
+
+  for(int k=1;k<=N;k++){
+    for(int i=1;i<=N;i++){
+      for(int j=1;j<=N;j++){
+        if(i==j) continue;
+        if(comp[i][k] && comp[k][j]){
+          comp[i][j] = 1;
+        }
+      }
+    }
+  }
+
+  for(int i=1;i<=N;i++){
+    for(int j=1;j<=N;j++){
+      if(comp[i][j]){
+        comp[j][i] = 1;
+      }
+    }
+  }
+
+  for(int i=1;i<=N;i++){
+    int answer = N;
+    for(int j=1;j<=N;j++){
+      if(comp[i][j]){
+        answer--;
+      }
+    }
+    cout << answer << "\n";
+  }
+}
