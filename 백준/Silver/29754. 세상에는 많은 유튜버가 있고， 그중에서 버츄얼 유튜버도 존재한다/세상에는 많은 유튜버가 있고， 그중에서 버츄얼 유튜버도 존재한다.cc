8812df7c20288f8b533edee4ca95350d0c33099b
윤환 <93152240+Yunan31@users.noreply.github.com>
2023-10-14 16:14:06 +0900

    [Silver I] Title: 세상에는 많은 유튜버가 있고, 그중에서 버츄얼 유튜버도 존재한다, Time: 92 ms, Memory: 3208 KB -BaekjoonHub

---
+++ 백준/Silver/29754. 세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다/세상에는 많은 유튜버가 있고， 그중에서 버츄얼 유튜버도 존재한다.cc
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+
+using namespace std;
+
+int N, M;
+vector<tuple<int, int, int>> stream;
+vector<string> names;
+int real[105] = {0};
+
+int main() {
+  cin >> N >> M;
+
+  for(int i=0;i<N;i++){
+    string name, start, end;
+    int day, st, ed, idx = names.size();
+
+    cin >> name >> day >> start >> end;
+    day = (day-1)/7;
+    st = stoi(start.substr(0, 2))*60 + stoi(start.substr(3, 2));
+    ed = stoi(end.substr(0, 2))*60 + stoi(end.substr(3, 2));
+
+    for(int j=0;j<names.size();j++){
+      if(names[j].compare(name) == 0){
+        idx = j;
+        break;
+      }
+    }
+
+    if(idx == names.size()) names.push_back(name);
+
+    stream.push_back({day, idx, ed-st});
+  }
+
+  sort(stream.begin(), stream.end());
+
+  // for(int i=0;i<stream.size();i++){
+  //   cout << get<0>(stream[i]) << " " << get<1>(stream[i]) << " " << get<2>(stream[i]) << endl;
+  // }
+
+  int now = 0;
+  for(int i=0;i<M/7;i++){
+    int stream_time[101] = {0};
+    int stream_day[101] = {0};
+    
+    while(get<0>(stream[now]) == i && now < N){
+      stream_time[get<1>(stream[now])] += get<2>(stream[now]);
+      stream_day[get<1>(stream[now])]++;
+      now++;
+    }
+
+    for(int k=0;k<names.size();k++){
+      //cout << names[k] << " " << stream_time[k] << endl;
+      if(stream_time[k] >= 60*60 && stream_day[k] >= 5){
+        real[k]++;
+      }
+    }
+  }
+
+  vector<string> answer;
+  for(int i=0;i<names.size();i++){
+    if(real[i] == M/7){
+      answer.push_back(names[i]);
+    }
+  }
+
+  if(answer.size() == 0) cout << -1;
+  else{
+    sort(answer.begin(), answer.end());
+    for(int i=0;i<answer.size();i++){
+      cout << answer[i] << "\n";
+    }
+  }
+}
