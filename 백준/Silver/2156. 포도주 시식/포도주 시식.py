7df55260f6d9e6eb668f14308f77f653c80961fb
윤환 <93152240+Yunan31@users.noreply.github.com>
2024-01-21 13:06:38 +0900

    [Silver I] Title: 포도주 시식, Time: 416 ms, Memory: 33164 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -4,8 +4,8 @@ wines = [int(input()) for _ in range(n)]
 DP = [[0]*3 for _ in range(n+1)]
 
 for i in range(n):
-  for drink in range(2):
-    DP[i+1][drink+1] = DP[i][drink] + wines[i]
+  DP[i+1][1] = DP[i][0] + wines[i]
+  DP[i+1][2] = DP[i][1] + wines[i]
   DP[i+1][0] = max(DP[i])
   
 print(max(DP[n]))
